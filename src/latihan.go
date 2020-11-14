cf2c467bdd527572a0c09c9d731c23ef541a3cda
rysamuel9 <ronysamuel19@gmail.com>
2020-11-15 00:24:10 +0700

    New

--- src/latihan.go
+++ src/latihan.go
@@ -7,8 +7,8 @@ func main() {
 	var nama = "Rony Samuel Tamba" //variabel dan tipe data
 
 	fmt.Println("Hello World, My name is", nama, ", im", umur) //Cetak data
-	fmt.Printf("Number : %b", 100)                             //Print console fmt
+	fmt.Printf("Number : %b", 100)                             //Print console fmt binary
 	fmt.Println()
-	fmt.Printf("This is : %t", false) //Print console fmt
+	fmt.Printf("This is : %t", false) //Print console fmt boolean
 	fmt.Println()
 }
