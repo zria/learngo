2a7e0f872c56e0daac8e912066f8595e032879a0
rysamuel9 <ronysamuel19@gmail.com>
2020-11-15 00:21:48 +0700

    new update

--- src/latihan.go
+++ src/latihan.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import "fmt" //package fmt untuk input/output
 
 func main() {
-	var umur = 21
+	var umur = 21                  //variabel dan tipe data
+	var nama = "Rony Samuel Tamba" //variabel dan tipe data
 
-	fmt.Println("Hello World, My name is Rony Samuel Tamba, Im", umur, "y.o")
+	fmt.Println("Hello World, My name is", nama, ", im", umur) //Cetak data
+	fmt.Printf("Number : %b", 100)                             //Print console fmt
+	fmt.Println()
+	fmt.Printf("This is : %t", false) //Print console fmt
+	fmt.Println()
 }
